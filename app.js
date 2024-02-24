dd0ab62c271238d9c62d813ae57b5c52a876b6f4
mostaq786 <60430930+mostaq786@users.noreply.github.com>
2024-02-24 22:38:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ http.createServer(function (req, res) {
 		}); 
 	} 
 	
-}).listen(3000, function () { 
-	console.log("SERVER STARTED PORT: 3000"); 
+}).listen(port, function () { 
+	console.log("SERVER STARTED PORT: 8080"); 
 }); 
